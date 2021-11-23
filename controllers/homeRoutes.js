4aa1e5a0fa4eeffd87f3e119d64ef25b617eb188
TauH <thaywood988@gmail.com>
2021-11-23 18:32:43 +1100

    tue

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const { User } = require("../models");
+router.get("/", async (req, res) => {
+  try {
+    res.render("homepage", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", async (req, res) => {
+  try {
+    res.render("login", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
