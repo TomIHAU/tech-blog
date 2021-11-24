bb32f168e6a5fcb1342618b7ca99bda90fb6afa6
TauH <thaywood988@gmail.com>
2021-11-24 12:23:15 +1100

    finish model relationships?

--- models/index.js
+++ models/index.js
@@ -1,3 +1,31 @@
 const Comment = require("./Comment");
 const Post = require("./Post");
 const User = require("./User");
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "cascade",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "cascade",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
